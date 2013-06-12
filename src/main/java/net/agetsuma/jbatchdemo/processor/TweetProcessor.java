e03ea78be776406f073ff1f20c40f5ce7c867086
Norito Agetsuma <norito.agetsuma@gmail.com>
2013-06-13 03:03:02 +0900

    README追記。

--- src/main/java/net/agetsuma/jbatchdemo/processor/TweetProcessor.java
+++ src/main/java/net/agetsuma/jbatchdemo/processor/TweetProcessor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.agetsuma.jbatchdemo.processor;
 
 import javax.batch.api.chunk.ItemProcessor;
